d7cf300774ee4814c9c19390f63500ab7d9f84b0
manuel <manuel@manuel-lenovo>
2018-05-11 10:05:34 +0200

    Commit inicial

---
+++ main.c
@@ -0,0 +1,20 @@
+/* 
+ * @file main.c
+ * @author: Manuel Alférez Ruiz
+ * @date 11 de mayo de 2018, 10:00
+ * @note Problema 7
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * @brief Función principal
+ * @param argc Número de parámetros por línea de ordenes
+ * @param argv Parámetros por líneas de órdenes
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
