d0e3825fd2894156346469fd67f040a9efa83c58
manuel <manuel@manuel-lenovo>
2018-05-11 10:12:31 +0200

    Enunciado añadido

--- main.c
+++ main.c
@@ -5,6 +5,22 @@
  * @note Problema 7
  */
 
+/**
+ * ENUNCIADO DEL PROBLEMA:
+ * En una guerra, los ejércitos enemigos han invadido n ciudades. Los servicios 
+ * de inteligencia están informados de que en cada una de las ciudades invadidas
+ * se encuentran e efectivos enemigos. Para contraatacar, el Grupo de Interven-
+ * ción Rápida dispone de n equipos listos para intervenir. Cada uno de ellos 
+ * consta de d efectivos completamente equipados y entrenados. Para garantizar el 
+ * éxito de la intervención en una ciudad es necesario que se cuenten al menos 
+ * con tantos efectivos de defensa como el enemigo. 
+ * 
+ * Describir e implementar al menos dos estrategias voraces (distintas funciones
+ * de selección) que indiquen qué equipo de intervención debe ir a cada ciudad,
+ * de forma que se maximice el número de éxitos garantizados. 
+ */
+
+
 #include <stdio.h>
 #include <stdlib.h>
 
