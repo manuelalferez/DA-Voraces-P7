082ea9ebb51824adeede009335effe310bf51d01
manuel <manuel@manuel-lenovo>
2018-05-11 17:11:49 +0200

    Primera solución (sin comprobar funcionamiento)

--- main.c
+++ main.c
@@ -5,6 +5,9 @@
  * @note Problema 7
  */
 
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * ENUNCIADO DEL PROBLEMA:
  * En una guerra, los ejércitos enemigos han invadido n ciudades. Los servicios 
@@ -20,9 +23,44 @@
  * de forma que se maximice el número de éxitos garantizados. 
  */
 
+int seleccion(int ciudades[], int equipos[], int ciudad_actual, int tam) {
+    int equipo = 0; //Equipo a enviar
+    if (equipos[equipo] >= ciudades[ciudad_actual]) return equipo;
+    else return equipo++;
+}
 
-#include <stdio.h>
-#include <stdlib.h>
+void eliminar(int v[], int pos, int n) {
+    v[pos] = v[n - 1];
+}
+
+int factible(int vector[], int n) {
+    if (vector[n - 1] == 0)
+        return 1;
+    else return 0;
+}
+
+int* algoritmoVoraz(int ciudades[], int equipos[], int n) {
+    int* S = malloc(n * sizeof(int)); //S es el conjunto solución
+    //Inicializamos
+    for (int i = 0; i < n; i++) S[i] = 0;
+    //Variables
+    int tam_S, tam_equipos, x, ciudad_actual;
+    ciudad_actual = 0;
+    tam_S=0;
+    tam_equipos = n;
+
+    //Bucle voraz
+    while (tam_S != n && tam_equipos != 0) {
+        x = seleccion(ciudades, equipos, ciudad_actual, tam_equipos);
+        x = equipos[x];
+        eliminar(equipos,x, tam_equipos);
+        tam_equipos--;
+        if(factible(S, tam_S)){
+            S[tam_S]=x;
+        }
+    }
+    return S;
+}
 
 /*
  * @brief Función principal
